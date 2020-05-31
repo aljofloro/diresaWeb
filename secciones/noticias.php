154cdbe0156ed42d76018d6cc3dabdb9ac323dc0
aljofloro <aljofloro@gmail.com>
2020-05-31 02:52:33 -0500

    Noticia-Api

--- secciones/noticias.php
+++ secciones/noticias.php
@@ -10,68 +10,43 @@
 			<?php
 			$Configuracion = Configuracion::getConfiguracion();
 			$jsonq = json_decode(file_get_contents($Configuracion->get("SERVER_API_PORTAL").$Configuracion->get("GET_NOTICIAS_MAIN"),true));
-			$datos = $jsonq->data;
+			$error = $jsonq->error;
+			if($error){
+				print_r($jsonq->mensaje);
+			}else{
+				$noticias = $jsonq->data;
+				foreach ($noticias as $noticia) {
+					$idNoticia = $noticia->id_noticia;
+					$idTipoNoticia = $noticia->id_tipo_noticia;
+					$fecha = $noticia->fecha;
+					$titulo = $noticia->titular;
+					$entrada = $noticia->entradilla;
+					$urlImagen = $noticia->imagen;
+					$tipoNoticia = $noticia->tipo_noticia;
+					$nombreTipoNoticia = $tipoNoticia->nombre;
+					$date = date_create($fecha);
+					?>
+				<div class="item">
+					<div class="noticias_descr">
+						<div class="noticias_descr_image">
+							<img src="<?php echo $Configuracion->get("SERVER_IMG").$urlImagen; ?>" />
+						</div>
+						<div class="noticias_descr_body">
+							<h4 class="noticias_descr_title"><?php echo $titulo; ?></h4>
+							<div class="noticias_descr_fecha"><?php echo $nombreTipoNoticia." // ".date_format($date,"l, d/m/Y");  ?></div>
+							<div class="noticias_descr_text">
+								<p><?php echo $entrada; ?></p>
+							</div>
+							<div class="noticias_descr_footer">
+								<p><a class="btn" href="#" role="button">LEER MAS</a></p>
+							</div>
+						</div>
+					</div>
+				</div>	
+					<?php
+				}
+			}
 			?>
-				<div class="item">
-					<div class="noticias_descr">
-						<div class="noticias_descr_image">
-							<img src="img/noticias/hostpitalUnanue.png" />
-						</div>
-						<div class="noticias_descr_body">
-							<h4 class="noticias_descr_title">HOSPITAL HIPÓLITO UNANUE Y ESTABLECIMIENTOS EN
-								ALERTA
-								ANTE CORONAVIRUS</h4>
-							<div class="noticias_descr_fecha">Regional // Lun. 10 / Feb / 2020</div>
-							<div class="noticias_descr_text">
-								<p>El Director Ejecutivo de Epidemiología de la Diresa Tacna Dr.
-									Edgar Tejada Vásquez manifestó que el hospital Hipólit.</p>
-							</div>
-							<div class="noticias_descr_footer">
-								<p><a class="btn" href="#" role="button">LEER MAS</a></p>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="item">
-					<div class="noticias_descr">
-						<div class="noticias_descr_image">
-							<img src="img/noticias/hostpitalUnanue.png" />
-						</div>
-						<div class="noticias_descr_body">
-							<h4 class="noticias_descr_title">HOSPITAL HIPÓLITO UNANUE Y ESTABLECIMIENTOS EN
-								ALERTA
-								ANTE CORONAVIRUS</h4>
-							<div class="noticias_descr_fecha">Regional // Lun. 10 / Feb / 2020</div>
-							<div class="noticias_descr_text">
-								<p>El Director Ejecutivo de Epidemiología de la Diresa Tacna Dr.
-									Edgar Tejada Vásquez manifestó que el hospital Hipólit.</p>
-							</div>
-							<div class="noticias_descr_footer">
-								<p><a class="btn" href="#" role="button">LEER MAS</a></p>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="item">
-					<div class="noticias_descr">
-						<div class="noticias_descr_image">
-							<img src="img/noticias/hostpitalUnanue.png" />
-						</div>
-						<div class="noticias_descr_body">
-							<h4 class="noticias_descr_title">HOSPITAL HIPÓLITO UNANUE Y ESTABLECIMIENTOS EN
-								ALERTA
-								ANTE CORONAVIRUS</h4>
-							<div class="noticias_descr_fecha">Regional // Lun. 10 / Feb / 2020</div>
-							<div class="noticias_descr_text">
-								<p>El Director Ejecutivo de Epidemiología de la Diresa Tacna Dr.
-									Edgar Tejada Vásquez manifestó que el hospital Hipólit.</p>
-							</div>
-							<div class="noticias_descr_footer">
-								<p><a class="btn" href="#" role="button">LEER MAS</a></p>
-							</div>
-						</div>
-					</div>
-				</div>
 			</div>
 		</div>
 	</div>
