a8cf13ae2134ae0051f8e14edf3323fc7ef78af7
aljofloro <aljofloro@gmail.com>
2020-05-31 01:04:57 -0500

    slider.APi

--- config/config.php
+++ config/config.php
@@ -5,4 +5,3 @@ $Configuracion->set('SERVER_API_PORTAL','http://api.proydir.nom.pe/src/Portal/')
 $Configuracion->set('SERVER_IMG','http://api.proydir.nom.pe/');
 $Configuracion->set('GET_BANNER','BannerListar.php');
 ?>
-?>
--- secciones/slider.php
+++ secciones/slider.php
@@ -1,18 +1,18 @@
+<div class="owl-carousel owl-theme" id="main-slider">
 <?php
 	$Configuracion = Configuracion::getConfiguracion();
-	
+	$jsonq = json_decode(file_get_contents($Configuracion->get("SERVER_API_PORTAL").$Configuracion->get("GET_BANNER"),true));
+	$datos = $jsonq->data;
+	foreach ($datos as $banner) {
+		$idBanner = $banner->id_banner;
+		$urlImagen = $banner->imagen;
+		$ref = $banner->url;
+		?>
+	<div>
+		<img class="bd-placeholder-img" width="100%" height="100%" src="<?php echo $Configuracion->get("SERVER_IMG").$urlImagen; ?>"
+			alt="First slide">
+	</div>
+		<?php
+	}
 ?>
-<div class="owl-carousel owl-theme" id="main-slider">
-	<div>
-		<img class="bd-placeholder-img" width="100%" height="100%" src="img/fondo.png"
-			alt="First slide">
-	</div>
-	<div>
-		<img class="bd-placeholder-img" width="100%" height="100%" src="img/fondo.png"
-			alt="First slide">
-	</div>
-	<div>
-		<img class="bd-placeholder-img" width="100%" height="100%" src="img/fondo.png"
-			alt="First slide">
-	</div>
 </div>
