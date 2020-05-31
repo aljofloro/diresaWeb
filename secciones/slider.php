be42c8d0d0d6f7b80e7d05df40b2590b7973ca52
aljofloro <aljofloro@gmail.com>
2020-05-31 01:50:12 -0500

    slider

--- secciones/slider.php
+++ secciones/slider.php
@@ -3,19 +3,22 @@
 	$Configuracion = Configuracion::getConfiguracion();
 	$jsonq = json_decode(file_get_contents($Configuracion->get("SERVER_API_PORTAL").$Configuracion->get("GET_BANNER"),true));
 	$error = $jsonq->error;
-	print_r($error);
-	$datos = $jsonq->data;
+	if($error){
+		print_r($jsonq->mensaje);
+	}else{
+		$datos = $jsonq->data;
 	
-	foreach ($datos as $banner) {
-		$idBanner = $banner->id_banner;
-		$urlImagen = $banner->imagen;
-		$ref = $banner->url;
-		?>
-	<div>
-		<img class="bd-placeholder-img" width="100%" height="100%" src="<?php echo $Configuracion->get("SERVER_IMG").$urlImagen; ?>"
-			alt="First slide">
-	</div>
-		<?php
-	}
+		foreach ($datos as $banner) {
+			$idBanner = $banner->id_banner;
+			$urlImagen = $banner->imagen;
+			$ref = $banner->url;
+			?>
+		<div>
+			<img class="bd-placeholder-img" width="100%" height="100%" src="<?php echo $Configuracion->get("SERVER_IMG").$urlImagen; ?>"
+				alt="First slide">
+		</div>
+			<?php
+		}
+	}	
 ?>
 </div>
