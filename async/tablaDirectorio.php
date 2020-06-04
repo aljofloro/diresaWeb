0fe6316e87ffe681fdd1f8b20f1f3beef3a4bfb9
aljofloro <aljofloro@gmail.com>
2020-06-04 00:26:21 -0500

    tablaDirectorio

--- async/tablaDirectorio.php
+++ async/tablaDirectorio.php
@@ -10,8 +10,19 @@ if ($error) {
   $oficinas = $jsonq->data;
   $numero = 1;
   foreach ($oficinas as $oficina) {
-    $nombre = $oficina->nombre;
-    $directorio[] = array($numero,$nombre);
+    $detalle = '<p>'.$oficina->nombre.'</p><div class="row textoinfo">';
+    $personas = $oficina->encargados;
+    foreach ($personas as $persona) {
+      $detalle .= '<div class="col-md-4">';
+      $detalle .= '<p>'.$persona->titulo.' '.$persona->nombre_completo.'</br>';
+      $detalle .= $persona->cargo.'</br>';
+      $detalle .= 'Dirección: '.$persona->direccion.'</br>';
+      $detalle .= 'Correo electrónico: '.$persona->correo_electronico.'</br>';
+      $detalle .= 'Teléfono:'.$persona->numero_telefono.'</p>';
+      $detalle .= '</div>';
+    }
+    $detalle .='</div>';
+    $directorio[] = array(str_pad($numero,3,'0',STR_PAD_LEFT),$detalle);
     $numero ++;
   }
 }
