84291a65633fb8503cb918dabcd19dcf2f568828
aljofloro <aljofloro@gmail.com>
2020-06-05 19:42:35 -0500

    tabla

--- js/dataTable.init.js
+++ js/dataTable.init.js
@@ -45,11 +45,7 @@ $(document).ready(function() {
             }
         ]
     });
-=======
-  const NO_VIGENTE = 1;
-  const VIGENTE = 2;
-  const ANEXO = 3;
->>>>>>> 760f4d96d9cd1c6ac4cfff03c9725841c5d6eab5
+
   /*TABLA DE CONVOCATORIAS*/
   $('#convocatoria').DataTable({
     "destroy": true,
@@ -131,7 +127,6 @@ $(document).ready(function() {
           },
       ]
   });
->>>>>>> 1d3b4f670d1612c37f29368b4965b1acf9290d8d
 
     /*convocatoria sin anexo*/
     $('#convocatoriasinanexo').DataTable({
@@ -172,43 +167,11 @@ $(document).ready(function() {
         ]
     });
 
-<<<<<<< HEAD
     /*TABLA GENERAL*/
     $('#tablageneral').DataTable({
         "ordering": false,
         "language": {
             "url": "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
-=======
-  /** TABLA CONVOCATORIA GENERICA */
-  dataTableConvocatoriaInit();
-  function dataTableConvocatoriaInit(valor){
-    var vigencia = valor || '';
-    var params = $('#tablaConvocatoria').attr("data-params");
-    $('#tablaConvocatoria').DataTable({
-      "destroy": true,
-      "serverSide": false,
-      "scrollX": true,
-      "language": {
-        "url" : "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
-      },
-      "ajax": "async/tablaConvocatoria.php?"+params+"&vigencia="+vigencia,
-      "dom": '<"selectConvocatoria"><"#top_convocatoria"Bl>t<"bottom"ip><"clear">',
-      "buttons": [{
-        "text": '<button class="btn btn-convocatorias">Convocatorias Vigentes</button>',
-        "key": '1',
-        "action": function(e, dt, node, config) {
-          dataTableConvocatoriaInit(VIGENTE);
-        }
-      },
-      {
-        "text": '<button class="btn btn-convocatorias">Convocatorias Pasadas</button>',
-        "key": {
-            "shiftKey": true,
-            "key": '2'
-        },
-        action: function(e, dt, node, config) {
-          dataTableConvocatoriaInit(NO_VIGENTE);
->>>>>>> 1d3b4f670d1612c37f29368b4965b1acf9290d8d
         }
     });
 
