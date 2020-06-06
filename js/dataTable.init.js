924bd31219d1f269edc477e82d495828f3b8d124
aljofloro <aljofloro@gmail.com>
2020-06-05 19:53:29 -0500

    confilcto tablas

--- js/dataTable.init.js
+++ js/dataTable.init.js
@@ -46,88 +46,6 @@ $(document).ready(function() {
         ]
     });
 
-  /*TABLA DE CONVOCATORIAS*/
-  $('#convocatoria').DataTable({
-    "destroy": true,
-    "scrollX": true,
-    "ordering": false,
-    "language": {
-      "url": "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
-    },
-    "lengthMenu": [
-      [5, 10, 20, -1],
-      [5, 10, 20, "Todas"]
-    ],
-    "dom": '<"selectConvocatoria"><"#top_convocatoria"Bl>t<"bottom"ip><"clear">',
-    /* "dom": 'Bfrtip',*/
-    "buttons": [{
-      "text": '<button class="btn btn-convocatorias">Convocatorias Vigentes</button>',
-      "key": '1',
-      "action": function(e, dt, node, config) {
-          alert('Button 1 activated');
-      }
-    },
-    {
-      "text": '<button class="btn btn-convocatorias">Convocatorias Pasadas</button>',
-      "key": {
-          "shiftKey": true,
-          "key": '2'
-      },
-      action: function(e, dt, node, config) {
-          alert('Button 2 activated');
-      }
-    },
-    {
-      "text": '<button class="btn btn-convocatorias">Anexos</button>',
-      "key": {
-          "shiftKey": true,
-          "key": '3'
-      },
-      action: function(e, dt, node, config) {
-          alert('Button 3 activated');
-      }
-    }]
-  });
-
-  /*convocatoria sin anexo*/
-  $('#convocatoriasinanexo').DataTable({
-      "columnDefs": [{
-          "targets": [1, 2],
-          "width": "10em!important"
-      }, ],
-      "destroy": true,
-      "scrollX": true,
-      "ordering": false,
-      "language": {
-          "url": "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
-      },
-      "lengthMenu": [
-          [5, 10, 20, -1],
-          [5, 10, 20, "Todas"]
-      ],
-      "dom": '<"selectConvocatoria"><"#top_convocatoria"Bl>t<"bottom"ip><"clear">',
-
-      /* "dom": 'Bfrtip',*/
-      "buttons": [{
-              "text": '<button class="btn btn-convocatorias">Convocatorias Vigentes</button>',
-              "key": '1',
-              "action": function(e, dt, node, config) {
-                  alert('Button 1 activated');
-              }
-          },
-          {
-              "text": '<button class="btn btn-convocatorias">Convocatorias Pasadas</button>',
-              "key": {
-                  "shiftKey": true,
-                  "key": '2'
-              },
-              action: function(e, dt, node, config) {
-                  alert('Button 2 activated');
-              }
-          },
-      ]
-  });
-
     /*convocatoria sin anexo*/
     $('#convocatoriasinanexo').DataTable({
         "columnDefs": [{
@@ -189,16 +107,21 @@ $(document).ready(function() {
     dataTableConvocatoriaInit();
 
     function dataTableConvocatoriaInit(valor) {
+      var vigencia = valor || '';
         var params = $('#tablaConvocatoria').attr("data-params");
         $('#tablaConvocatoria').DataTable({
             "ordering": false,
             "destroy": true,
             "serverSide": false,
             "scrollX": true,
+            "lengthMenu": [
+              [5, 10, 20, -1],
+              [5, 10, 20, "Todas"]
+            ],
             "language": {
                 "url": "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
             },
-            "ajax": "async/tablaConvocatoria.php?" + params + "&vigencia=" + valor,
+            "ajax": "async/tablaConvocatoria.php?" + params + "&vigencia=" + vigencia,
             "dom": '<"selectConvocatoria"><"#top_convocatoria"Bl>t<"bottom"ip><"clear">',
             "buttons": [{
                     "text": '<button class="btn btn-convocatorias">Convocatorias Vigentes</button>',
@@ -230,5 +153,4 @@ $(document).ready(function() {
             ]
         });
     }
-
 });
