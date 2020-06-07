842912f27f7df395aea315137ba97d6618d67956
aljofloro <aljofloro@gmail.com>
2020-06-06 22:38:25 -0500

    actualizacion

--- secciones/convocatorias.php
+++ secciones/convocatorias.php
@@ -35,11 +35,6 @@ switch ($tipoConvocatoria) {
           <th class="linea">RESULTADO FINAL</th>
         </tr>
       </thead>
-<<<<<<< HEAD
-    </table>
-
-=======
     </table>    
->>>>>>> 327a87399ab4113cd527a9d88e822da783922f00
   </div>
 </div>
