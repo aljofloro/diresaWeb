81990df5293b0a33244e5081c8170ff56b4c3877
pieri2019 <pierinaa_10@hotmail.com>
2020-06-08 21:56:01 -0500

    agenda

--- agenda.php
+++ agenda.php
@@ -1,50 +1,39 @@
 <!DOCTYPE html>
-<html lang="en">
-<?php
-include_once("secciones/head.php");
-?>
+<html lang="es">
+
+<head>
+    <?php
+    require_once("config/config.php");
+    include_once("secciones/head.php");
+    include_once("secciones/estilos.php");
+    ?>
+</head>
+
 <body id="top">
-    <?php
-    include_once("secciones/header.php");
-    ?>
-    <?php
-    include_once("secciones/home.php");
-    ?>
-      <div class="site-section">
-        <div class="container">
-            <div class="row">
-                <div class="site-section-heading text-center mb-4 col-md-6 w-border mx-auto">
-                    <h2 class="mb-md-4">AGENDA</h2>
-                </div>
-            </div>
-            <table id="example" class=" table table-striped display " style="width:100%">
-                <thead class="thead-blue">
-                    <tr>
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody class="hola">
-                    <tr>
-                        <td>001</td>
-                        <td class="texto">Normas para el otorgamiento de viáticos y rendición de cuentas de los
-                            servidores y funcionarios en comisión de servicio en el gobierno regional de tacna</td>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
-    <?php
-    include_once("secciones/noticias.php");
-    ?>
-    <?php
-    include_once("secciones/partners2.php");
-    ?>
-    <?php
-    include_once("secciones/footer.php");
-    ?>
+    <header>
+        <?php
+        include_once("secciones/horario.php");
+        include_once("secciones/presentacion.php");
+        include_once("secciones/menu.php");
+        ?>
+    </header>
+    <main role="main">
+        <?php
+        include_once("secciones/slider.php");
+        $tipoConvocatoria = 2;
+        include_once("secciones/agenda.php");
+        include_once("secciones/noticias.php");
+        ?>
+        <?php
+        include_once("secciones/partners2.php");
+        ?>
+        <footer class="footer">
+            <?php
+            include_once("secciones/footer.php");
+            include_once("secciones/loader.php");
+            ?>
+        </footer>
+    </main>
 </body>
 
 </html>
---
+++ secciones/agenda.php
@@ -0,0 +1,32 @@
+<div class="site-section-seccion">
+    <div class="container">
+        <div class="row">
+            <div class="site-section-heading text-center mb-4 col-md-6 w-border mx-auto">
+                <h2 class="mb-md-4">AGENDA</h2>
+            </div>
+        </div>
+        <div class="form-group pmd-textfield pmd-textfield-floating-label">
+            <label class="control-label" for="datepicker">Select Date</label>
+            <input type="text" class="form-control" id="datepicker">
+        </div>
+        <table id="tablaagenda" class=" table table-striped display " style="width:100%">
+            <thead class="thead-blue">
+                <tr>
+                    <th class="linea text-center">N°</th>
+                    <th class="">Descripción</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="text-center">001</td>
+                    <td class="text-justify">
+                        <p class="tablaagendatitulo">FIRMA DE CONVENIO DE COOPERACIÓN INTERINSTITUCIONAL ENTRE EL PODER JUDICIAL Y LA DIRECCIÓN REGIONAL DE SALUD TACNA</p>
+                        <p class="textoinfo"><i class="fas fa-calendar-alt fa-sm"></i>Miércoles 05 de febrero de 2020 - 15:00 pm </p>
+                        <p class="textoinfo"><i class="fas fa-map-marker-alt fa-sm"></i>Presidencia de la corte superior de justicia de tacna</p>
+                        <p class="textoinfo"><i class="fas fa-pencil-alt fa-sm"></i>Corte Superior de Justicia de Tacna y Dirección Regional de Salud Tacna</p>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
